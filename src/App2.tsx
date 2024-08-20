b6a1f00dc065b17a54fd263e37a68909b817cfb0
Vera Miranda Luis Gustavo <luisvera550@gmail.com>
2024-08-20 07:05:37 -0400

    comit casi final 2

--- src/App2.tsx
+++ src/App2.tsx
@@ -7,8 +7,9 @@ import {   Route, Routes } from 'react-router-dom'
 export function App2() {
 return(
 <Routes>
-      <Route path='/scaner-prueba/:id' element={<App></App>}></Route>
+      <Route path='/scanner-prueba/:id' element={<App></App>}></Route>
       <Route path='/' element={<div>hola mundo</div>}></Route>
+      <Route path='/scanner-prueba/' element={<div>hola mundo</div>}></Route>
     </Routes>
 )
     
