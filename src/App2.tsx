f1f3b2950b028f51f0f909b2806d677b8afce694
Vera Miranda Luis Gustavo <luisvera550@gmail.com>
2024-08-20 07:34:20 -0400

    comit con todo solucionado

--- src/App2.tsx
+++ src/App2.tsx
@@ -7,15 +7,17 @@ import {   NavLink, Route, Routes } from 'react-router-dom'
 export function App2() {
 return(
 <>
-<NavLink to="/scanner-prueba/1234">
-      <button>
-        Go to scanner
-      </button>
-    </NavLink>
+
 <Routes>
       <Route path='/scanner-prueba/:id' element={<App></App>}></Route>
-      <Route path='/' element={<div>hola mundo</div>}></Route>
-      <Route path='/scanner-prueba/' element={<div>hola mundo</div>}></Route>
+      
+      <Route path='/scanner-prueba/' element={<NavLink to="/scanner-prueba/1234">
+      <button>
+        Presiona aui para ir al escanner
+      </button>
+    </NavLink>
+    
+    }></Route>
     </Routes>
     </>
 )
